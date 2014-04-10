ff85e3d3294f708153696cff5c025621a6483f5f
AldwynForTheWin <elephunkies101@gmail.com>
2014-04-11 07:25:30 +0800

    customized admin change list

--- booksys/admin.py
+++ booksys/admin.py
@@ -1,15 +1,31 @@
 from django.contrib import admin
 from booksys.models import Publisher, Department, Category, Author, Book
 
-# class BookSysAdmin(admin.ModelAdmin):
+class BookAdmin(admin.ModelAdmin):
+	list_display = ('title', 'pub_date', 'author', 'publisher','category')
+	list_filter = ['pub_date']
+	search_fields = ['title']
+
+class AuthorAdmin(admin.ModelAdmin):
+	list_display = ('lastname', 'firstname', 'middlename', 'department')
+
+class DepartmentAdmin(admin.ModelAdmin):
+	list_display = ('name', 'contact_no')
+
+class PublisherAdmin(admin.ModelAdmin):
+	list_display = ('name', 'address', 'contact_no')
+
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ('acronym', 'description')
+
 # 	fieldsets = [
 # 		('Edit Book Title', {'fields': ['title']}),
 # 		('Edit Book Infos', {'fields': ['pub_date', 'author', 'publisher', 'category'], 'classes': ['collapse']})
 # 	]
 # 	inlines = [AuthorInline, DepartmentInline, PublisherInline, CategoryInline]
 
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Department)
-admin.site.register(Publisher)
-admin.site.register(Category)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Publisher, PublisherAdmin)
+admin.site.register(Category, CategoryAdmin)
