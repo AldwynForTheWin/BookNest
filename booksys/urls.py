cb5907aa02a011dc18269d5f7ae07a8a4a5403fb
AldwynForTheWin <elephunkies101@gmail.com>
2014-04-11 08:24:24 +0800

    enabled shortcuts for rendering data to views

--- booksys/urls.py
+++ booksys/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, url
 from booksys import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index')
+	url(r'^$', views.index, name='index'),
+	url(r'^(?P<book_id>\d+)/$', views.detail, name='detail'),
+	url(r'^(?P<book_id>\d+)/results/$', views.results, name='results'),
 )
--- booksys/views.py
+++ booksys/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.template import RequestContext, loader
+from booksys.models import Book
 
 def index(request):
-	return HttpResponse("Hello word. You're at the booksys's index.")
+	latest_book_list = Book.objects.order_by('title')
+	return render(request, 'booksys/index.html', {'latest_book_list': latest_book_list})
+
+def detail(request, book_id):
+	book = get_object_or_404(Book, pk=book_id)
+	return render(request, 'booksys/detail.html', {'book': book})
+
+def results(request, book_id):
+	return HttpResponse("You're looking at the results of %s." % book_id)
