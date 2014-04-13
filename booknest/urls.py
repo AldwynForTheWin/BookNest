5bc6d24f65964ae2a9e46e164f3898dbf6f534a0
AldwynForTheWin <elephunkies101@gmail.com>
2014-04-13 11:14:33 +0800

    diplay of book details enabled; EOE

--- booknest/urls.py
+++ booknest/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'booknest.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^booksys/', include('booksys.urls')),
+    url(r'^booksys/', include('booksys.urls', namespace='booksys')),
     url(r'^admin/', include(admin.site.urls)),
 )
